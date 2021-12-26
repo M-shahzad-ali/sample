c6de3e874f081606588407943f7b4851a49b53e6
shahzadali <64818830+M-shahzad-ali@users.noreply.github.com>
2021-12-27 01:42:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// var mainImage=document.getElementById("main_image");
+// var childImage=document.getElementById("first_image")
+
+// // childImage[0].onclick=function(){
+// //     mainImage.src=childImage[0].src;
+// //     console.log(mainImage.src)
+// // }
+// // childImage[1].onclick=function(){
+// //     mainImage.src=childImage[1].src;
+// // }
+// // childImage[2].onclick=function(){
+// //     mainImage.src=childImage[2].src;
+// // }
+// // childImage[3].onclick=function(){
+// //     mainImage.src=childImage[3].src;
+// // }
+
+
+// function change(){
+//     mainImage.src=childImage.src;
+
+// }
+
+
+// var firstImage=document.getElementById("first-image");
+// var secondImage=document.getElementsByClassName("second-image");
+// secondImage[0].onclick=function(){
+//     firstImage.src=secondImage[0].src;
+//     console.log(firstImage.src);
+// }
+
+// var First=document.getElementById("Product_image");
+// var second=document.getElementById("second-image");
+// function turn(){
+//     console.log(First)
+// }
+
+
+
+
+// function shahzaib(){
+//    var secsrc = this.img.src;
+//     first.src= 
+// }
